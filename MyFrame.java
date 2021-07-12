741d8712cb53f9c1fdb6d4744e1abb11a94413a3
anhquyen18 <anhquyen18092000@gmail.com>
2021-07-12 17:02:45 +0700

    add initial interface

--- MyFrame.java
+++ MyFrame.java
@@ -1,9 +1,12 @@
-
-public class MyFrame {
-
-	public static void main(String[] args) {
-
-		new MyFrame();
+import javax.swing.*;
+import java.awt.*;
+public class MyFrame extends JFrame {
+	MyFrame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("My Clock Program");
+		this.setLayout(new FlowLayout());
+		this.setSize(350, 200);
+		
+		this.setVisible(true);
 	}
-
 }
